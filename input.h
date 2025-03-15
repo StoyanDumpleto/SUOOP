2cced7d343679b0953a0367262759d696fd59652
StoyanDumpleto <147829352+StoyanDumpleto@users.noreply.github.com>
2025-03-15 22:30:26 +0200

    input.h

--- input.h
+++ input.h
@@ -3,44 +3,74 @@
 
 #include <iostream>
 #include <map>
+#include <string>
+#include <sstream>
+
 
 typedef std::map<size_t, Book> Books;
 extern Books books;
 
 template<class T>
-void read(std::map<size_t, T>& inBook, std::istream& istr)
+void read(std::map<size_t, T>& inBook, std::istream& is)
 {
-	std::string line;
-	getline(istr, line);
-	while (line != "end")
+	while (true)
 	{
-		std::istringstream istr1(line);
-		size_t id;
+		std::string id;
+		getline(is, id);
+		if (id == "end")
+			break;
+		std::istringstream istr(id);
+		size_t idx;
+		istr >> idx;
+		std::string author;
+		std::string title;
+		getline(is, author);
+		getline(is, title);
+		std::istringstream temp(author + "\n" + title);
+		inBook[idx] = Book(idx, temp);
+		auto itr = inBook.find(idx);
+		itr->second.setBorrowed("available");
+	}
+}
 
-		istr >> id;
-		T Book_(id, istr1);
-		inBook[id] = Book_ ;	
-	}	    
-};
-
-template<typename Key>
-void print(Key &idx, std::ostream& ostr)
+template<typename TBook>
+void print( TBook & book, std::ostream& ostr)
 {
+	std::string status = book.getBorrower();
+	ostr << "book "  << book.getId() << " \"" << book.getTitle() << "\"" << " by \"" 
+	     << book.getAuthor() << "\" " << "is \"" << status << "\"" << endl;
 };
 
-template<>
-void print<std::map<size_t, Book>*>(std::map<size_t, Book>* & _books, std::ostream& ostr)
+template<typename Т>
+void print(std::map<size_t, Book>* _books, std::ostream& ostr)
 {
-};
+	ostr << "-----" << endl;
+	ostr << "total books: " << _books->size() << endl;
+	for (auto itr = _books->begin(); itr != _books->end(); ++itr)
+	{
+		ostr << "book " << itr->first << " \"" << itr->second.getTitle() << "\"" << " by \""
+			<< itr->second.getAuthor() << "\" " << "is \"" << itr->second.getBorrower() << "\"" << endl;
+	}
+}; 
 
-template<typename Key>
-void borrow(Key &idx, std::string& _borrower)
+template<typename TBook>
+void borrow(TBook& book, std::string& _borrower)
 {
+	std::string status = "borrowed by " + _borrower;
+	book.setBorrowed(status);
+	cout << "book " << book.getId() << " \"" << book.getTitle() << "\"" << " by" << " \""
+		 << book.getAuthor() << "\" " << "is \"" << status << "\"" << endl;
 };
 
-template<typename Key>
-void borrow(Key &idx)
-{	
+template<typename TBook>
+void borrow(TBook& book)
+{
+	std::string status = "available";
+	book.setBorrowed(status);
+	cout << "book " << book.getId() << " \"" << book.getTitle() << "\"" << " by \""
+		 << book.getAuthor() << "\" " << "is \"" << status << "\"" << endl;
 };
 
 #endif
+
+
